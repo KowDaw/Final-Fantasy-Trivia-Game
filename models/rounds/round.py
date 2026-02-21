0071591d182da7a0bf96472027196c908c5fed52
KowDaw <kov.dav.23@gmail.com>
2026-02-21 20:32:06 +0100

    rewrite data to round_data

--- models/rounds/round.py
+++ models/rounds/round.py
@@ -1,5 +1,5 @@
 class Round:
-    def __init__(self, data):
-        self.question = data["question"]
-        self.answers = data["answers"]
-        self.right_answer = data["right_answer"]
+    def __init__(self, round_data):
+        self.question = round_data["question"]
+        self.answers = round_data["answers"]
+        self.right_answer = round_data["right_answer"]
