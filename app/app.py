17ffdf0848884290e92ea0b96439e7f3eba5bef8
KowDaw <kov.dav.23@gmail.com>
2026-02-22 13:30:27 +0100

    add some type hint to the app.py

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ from models.screens.rules_screen import RulesScreen
 from models.screens.options_screen import OptionsScreen
 from models.screens.final_fantasy_selector_screen import FinalFantasySelectorScreen
 from models.screens.game_screen import GameScreen
+from models.screens.screen import Screen
 
 class MainApp(Tk):
     def __init__(self):
@@ -19,7 +20,7 @@ class MainApp(Tk):
         self.attributes("-fullscreen", True)
         self.config(bg="black")
         self.is_fullscreen_on = True
-        self.current_screen = None
+        self.current_screen: Screen = None
         self.player_score = 0
 
         # callings
@@ -33,7 +34,7 @@ class MainApp(Tk):
         if self.current_screen:
             self.load_screen(self.current_screen_name)
 
-    def load_screen(self, screen_name, title_of_choosen_final_fantasy=None):
+    def load_screen(self, screen_name: str, title_of_choosen_final_fantasy=None):
         self.current_screen_name = screen_name
 
         if self.current_screen is not None:
