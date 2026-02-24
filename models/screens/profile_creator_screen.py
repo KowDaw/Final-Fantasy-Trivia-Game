859c6e176598d2be2098cf0313d9663d511d6f99
KowDaw <kov.dav.23@gmail.com>
2026-02-24 12:44:54 +0100

    add properties to ProfileCreatorScreen

--- models/screens/profile_creator_screen.py
+++ models/screens/profile_creator_screen.py
@@ -3,6 +3,9 @@ from models.screens.screen import Screen
 class ProfileCreatorScreen(Screen):
     def __init__(self, master):
         super().__init__(master)
+        self.alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ")
+        self.digits = "0 1 2 3 4 5 6 7 8 9".split(" ")
+        self.profile_name_under_creation = ""
 
         # callings
         self.display_background_image("assets/images/midgar-wallpaper.png")
